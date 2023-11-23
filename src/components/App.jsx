9c2a0616b94e1a92873a8cb7939621b2a5c84973
MariiaSvitsa <mariia0198@gmail.com>
2023-11-23 14:02:28 +0200

    +

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,3 +14,51 @@ export const App = () => {
     </div>
   );
 };
+
+
+// index.js
+// import { BrowserRouter } from "react-router-dom";
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </React.StrictMode>
+// );
+
+// import { Routes, Route } from "react-router-dom";
+// import Home from "path/to/pages/Home";
+// import About from "path/to/pages/About";
+// import Products from "path/to/pages/Products";
+
+// export const App = () => {
+//   return (
+//     <div>
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/about" element={<About />} />
+//         <Route path="/products" element={<Products />} />
+//       </Routes>
+//     </div>
+//   );
+// };
+
+// import { Routes, Route } from "react-router-dom";
+// import Home from "path/to/pages/Home";
+// import About from "path/to/pages/About";
+// import Products from "path/to/pages/Products";
+// import NotFound from "path/to/pages/NotFound";
+
+// const App = () => {
+//   return (
+//     <div>
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/about" element={<About />} />
+//         <Route path="/products" element={<Products />} />
+//         <Route path="*" element={<NotFound />} />
+//       </Routes>
+//     </div>
+//   );
+// };
--- src/index.js
+++ src/index.js
@@ -8,3 +8,14 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>
 );
+
+// index.js
+// import { BrowserRouter } from "react-router-dom";
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </React.StrictMode>
+// );
